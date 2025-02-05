b7d1e9d29e64233453e24b59f5fee7c2d9afce12
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2025-02-05 21:00:44 +0200

    Added date display and edit/delete buttons to Card

--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ import Card from './components/Card';
 function App() {
   return (
     <div className="cards">
-      <Card title="Card 1" description="This is the first card" />
-      <Card title="Card 2" description="This is the second card" />
-      <Card title="Card 3" description="This is the third card" />
+      <Card title="Card 1" description="This is the first card" date="10/01/2025"/>
+      <Card title="Card 2" description="This is the second card" date="10/01/2025"/>
+      <Card title="Card 3" description="This is the third card" date="10/01/2025"/>
     </div>
   );
 }
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,10 +1,19 @@
 import "../styles/Card.css";
+import { BsFillTrashFill } from "react-icons/bs";
+import { BsPencilSquare } from "react-icons/bs";
 
-const Card = ({title, description}) => {
+const Card = ({ title, description, date }) => {
     return (
         <div className="card">
             <h2>{title}</h2>
             <p>{description}</p>
+            <p>{date}</p>
+            <button className="edit-btn">
+                <BsPencilSquare />
+            </button>
+            <button className="delete-btn">
+                <BsFillTrashFill />
+            </button>
         </div>
     )
 }
