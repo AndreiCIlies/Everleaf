8707e70f98df45c89699a1779f3720a159e3c9a5
AndreiCIlies <116495791+AndreiCIlies@users.noreply.github.com>
2025-02-07 22:42:00 +0200

    Added Header component with styles and integrated it into App

--- src/App.js
+++ src/App.js
@@ -1,13 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Header from './components/Header';
 import Card from './components/Card';
 
 function App() {
   return (
-    <div className="cards">
-      <Card title="Card 1" description="This is the first card" date="10/01/2025"/>
-      <Card title="Card 2" description="This is the second card" date="10/01/2025"/>
-      <Card title="Card 3" description="This is the third card" date="10/01/2025"/>
+    <div className="app">
+      <div className="header">
+      <Header/>
+      </div>
+      <div className="cards">
+        <Card title="Card 1" description="This is the first card" date="10/01/2025"/>
+        <Card title="Card 2" description="This is the second card" date="10/01/2025"/>
+        <Card title="Card 3" description="This is the third card" date="10/01/2025"/>
+      </div>
     </div>
   );
 }
---
+++ src/components/Header.js
@@ -0,0 +1,11 @@
+import "../styles/Header.css";
+
+const Header = () => {
+    return (
+        <div className="header">
+            EVERLEAF
+        </div>
+    )
+}
+
+export default Header;
